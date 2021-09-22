aaef948be4811418955ed300013a1885436896cf
Braden Byrnes <mattbyrnes18@gmail.com>
2021-09-22 08:25:31 -0600

    made toString() Method that returns a description of the chatbot

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -2,9 +2,18 @@ package chat.model;
 
 public class Chatbot 
 {
-
+	private String name;
+	
 	public Chatbot()
 	{
-		
+		this.name = "placeholder";
+	}
+	
+	@Override
+	public String toString()
+	{
+		String description = "I am a chat bot!";
+		description += "Ask me a question!";
+		return description;
 	}
 }
