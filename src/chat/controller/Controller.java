3e562d56cf9f105938510c9f896619e24b199e9b
Braden Byrnes <mattbyrnes18@gmail.com>
2021-09-22 08:25:08 -0600

    made simple scanner

--- src/chat/controller/Controller.java
+++ src/chat/controller/Controller.java
@@ -1,8 +1,13 @@
 package chat.controller;
 
+import java.util.*;
+
 import chat.model.Chatbot;
 public class Controller 
 {
+	private Scanner scan = new Scanner(System.in);
+	private Chatbot myChatbot;
+	
 	public Controller()
 	{
 		
