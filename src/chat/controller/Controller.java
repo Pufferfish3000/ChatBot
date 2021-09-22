ad3e264046192b358ee7c9fb2a9c8f8c22853748
Braden Byrnes <mattbyrnes18@gmail.com>
2021-09-22 08:32:34 -0600

    fixed null string error

--- src/chat/controller/Controller.java
+++ src/chat/controller/Controller.java
@@ -7,15 +7,14 @@ public class Controller
 {
 	private Scanner scan = new Scanner(System.in);
 	private Chatbot myChatbot;
-	
 	public Controller()
 	{
-	this.myChatbot = new Chatbot();	
+	this.myChatbot = new Chatbot("default name");	
 	}
 	
 	public void main()
 	{
-		
+		System.out.print(myChatbot);
 	}
 
 }
