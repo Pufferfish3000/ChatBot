f8d6297fe4917b1c3ad0e527f621424fc84306fa
Braden Byrnes <mattbyrnes18@gmail.com>
2021-09-27 09:41:02 -0600

    finalized getTime() method

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,5 +1,7 @@
 package chat.model;
 
+import java.time.*;
+
 public class Chatbot 
 {
 	private String name;
@@ -11,9 +13,20 @@ public class Chatbot
 	
 	public String processText(String text)
 	{
-		String response = ">";
-		response += text + "\n";
 		
+		String response = ">";
+		if (text.equalsIgnoreCase("date") )
+		{
+			response += getDate();
+		}
+		else if (text.equalsIgnoreCase("time"))
+		{
+			response += getTime();
+		}
+//		switch (text)
+//		{
+//		case : text = "date";
+//		}
 		return response;
 	}
 	
@@ -27,6 +40,71 @@ public class Chatbot
 	public void setBotName(String name)
 	{
 		this.name = name;
+	}
+	
+	public String getDate()
+	{
+		String date = "The date today is " ;
+		
+		LocalDateTime currentDate = LocalDateTime.now();
+		
+		String day = currentDate.getDayOfWeek().toString();
+		date += day + ", ";
+		
+		day = currentDate.getDayOfMonth() + " ";
+		date += day;
+		
+		String month = currentDate.getMonth().toString();
+		date += month;
+		
+		date += ", " + currentDate.getYear();
+		return date;
+	}
+	
+	public String getTime()
+	{
+		String time = "The time today is ";
+		LocalDateTime currentTime = LocalDateTime.now();
+		int minute = currentTime.getMinute();
+		int hour = currentTime.getHour();
+		
+		time += hour + ":" + minute;
+		return time;
+	}
+	
+	public String sayGreeting()
+	{
+		String botGreeting = "";
+		
+		return botGreeting;
+	}
+	
+	public String sayFarewell()
+	{
+		String botFarewell = "";
+		
+		return botFarewell;
+	}
+	
+	public String randomTopic() 
+	{
+		String botRandom = "";
+		
+		return botRandom;
+	}
+	
+	public boolean isPolite()
+	{
+		boolean botPolite = false;
+		
+		return botPolite;
+	}
+	
+	public boolean isPolitical()
+	{
+		boolean botPolitical = false;
+		
+		return botPolitical;
 	}
 	
 	@Override
